f7cca12730e7782afe04b810b331e31f1b1f9ec5
Wes <108998223+wesgunning@users.noreply.github.com>
2023-07-21 13:05:22 -0400

    Fix repeat error message for upperCase check

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const pwd = document.getElementById('pwd');
-pwd.addEventListener('input', compare);
-pwd.addEventListener('input', length);
 const confirm = document.getElementById('confirm');
-confirm.addEventListener('input', compare)
+pwd.addEventListener('input', compare);
+confirm.addEventListener('input', compare);
+pwd.addEventListener('input', length);
+pwd.addEventListener('input', upperCheck);
+pwd.addEventListener('input', symbolCheck);
 
 function compare() {
     if (pwd.value.length > 0 && confirm.value.length === pwd.value.length) {
         for (let i=0; i<confirm.value.length; i++) {
             if (confirm.value[i] !== pwd.value[i]) {
-                console.log('false');
                 pwd.setCustomValidity('Invalid');
                 confirm.setCustomValidity('Invalid');
-                return false;
+                if (Boolean(confirm.parentElement.lastChild.classList == undefined)) {
+                    const p = document.createElement('p');
+                    p.textContent = ' PASSWORDS DO NOT MATCH';
+                    p.classList.add('error');
+                    confirm.parentElement.appendChild(p);
+                    return false;
+                }
             }
         }
-        console.log('true')
         pwd.setCustomValidity('');
         confirm.setCustomValidity('');
+        if (confirm.parentElement.lastChild.classList.contains('error')) {
+            confirm.parentElement.removeChild(confirm.parentElement.lastChild);
+        }
         return true;
     }
     else {
@@ -26,9 +35,57 @@ function compare() {
     }
 }
 function length() {
-    
-    const p = document.createElement('p');
-    p.textContent = ' MUST BE AT LEAST 8 CHARACTERS';
-    p.classList.add('error', 'length');
-    pwd.parentElement.appendChild(p);
+    for (i=0; i<pwd.parentElement.children.length; i++) {
+        if (Boolean(pwd.parentElement.children[i].classList.contains('length'))) {
+            if (pwd.value.length > 7) {
+                pwd.parentElement.removeChild(pwd.parentElement.children[i]);
+            }
+            return;
+        }
+        else if (i == pwd.parentElement.children.length -1) {
+            const p = document.createElement('p');
+            p.textContent = ' MUST BE AT LEAST 8 CHARACTERS';
+            p.classList.add('error', 'length');
+            pwd.parentElement.appendChild(p);
+        }
+    }
 }
+function upperCheck() {
+    for (i=0; i<pwd.parentElement.children.length; i++) {
+        if(Boolean(pwd.parentElement.children[i].classList.contains('upper'))) {
+            for (j=0; j<pwd.value.length; j++) {
+                const char = pwd.value[j].toUpperCase();
+                const reg = /\w/i;
+                if (reg.test(char) && char === pwd.value[j]) {
+                    pwd.parentElement.removeChild(pwd.parentElement.children[i]);
+                    return;
+                }
+            }
+            return;
+        }
+        else if (i == pwd.parentElement.children.length -1) {
+            const up = document.createElement('p');
+            up.textContent = ' MUST CONTAIN AN UPPERCASE LETTER';
+            up.classList.add('error', 'upper');
+            pwd.parentElement.appendChild(up);
+        }
+    }
+}
+/* function symbolCheck() {
+    for (i=0; i<pwd.parentElement.children.length; i++) {
+        if(Boolean(pwd.parentElement.children[i].classList.contains('symbol'))) {
+            for (j=0; j<pwd.value.length; j++) {
+                const reg = /[^a-zA-Z0-9 \s]/;
+                if (reg.test(pwd.value[j])) {
+                    pwd.parentElement.removeChild(pwd.parentElement.children[i]);
+                }
+            }
+        }
+        else if (i == pwd.parentElement.children.length -1) {
+            const sym = document.createElement('p');
+            sym.textContent = ' MUST CONTAIN A SYMBOL';
+            sym.classList.add('error', 'symbol');
+            pwd.parentElement.appendChild(sym);
+        }
+    }
+} */
